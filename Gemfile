d7b306279e0ab184d484e7f0f9035d1678751d4f
yuuki920 <yuuki.k.1672@gmail.com>
2020-07-23 23:34:20 +0900

    カレンダーの表記を変更

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
 gem 'simple_calendar', '~> 2.0'
+gem 'rails-i18n', '~> 5.1'
--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,11 @@ module TaskApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'
+
+    #デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
   end
 end
